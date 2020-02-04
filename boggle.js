cfc523eceb50fad998e7f453a114809bfd52b4af
Koumpotis Christos <chkoumpotis@davidson.edu>
2020-02-04 11:48:05 -0800

    boggle with correctly named variables and randomRange function corrected

--- boggle.js
+++ boggle.js
@@ -1,9 +1,9 @@
 // let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']; alphabet[Math.floor(Math.random()*26)]
-let allArrays = ['AAEEGN', 'ELRTTY', 'AOOTTW',
+let dice = ['AAEEGN', 'ELRTTY', 'AOOTTW',
   'ABBJOO', 'EHRTVW', 'CIMOTU', 'DISTTY', 'EIOSST',
   'DELRVY', 'ACHOPS', 'HIMNQU', 'EEINSU', 'EEGHNW',
   'AFFKPS', 'HLNNRZ', 'DEILRX'];
-let shuffledArray = [];
+let shuffledDice = [];
 
 function newBoggleBoard() {
   return [
@@ -29,37 +29,38 @@ function printBoggleBoard(boggleBoard) {
 //   console.log(allArrays);
 // }
 
-function randomRange(length) {
-  return Math.floor(Math.random() * length - 1);
+function randomRange(arrayLength) {
+  return Math.floor(Math.random() * arrayLength);
 }
 
-function removeElement(allArrays, index) {
-  let item = allArrays.splice(index, 1)[0];
+function removeElement(selectedDice, index) {
+  let item = selectedDice.splice(index, 1)[0];
   return item;
 }
 
-function shuffle(allArrays) {
-  while (allArrays.length > 0) {
-    let randIndex = randomRange(allArrays.length);
-    let removedElem = removeElement(allArrays, randIndex);
-    shuffledArray.push(removedElem);
-   // return shuffledArray;
+function shuffle(selectedDice) {
+  while (dice.length > 0) {
+  // console.log(dice);
+    let randIndex = randomRange(dice.length);
+    let removedElem = removeElement(dice, randIndex);
+    shuffledDice.push(removedElem);
+  // return shuffledArray;
     // console.log(removedElem);
   }
-  return shuffledArray;
+  return shuffledDice;
 }
 
 function shake(board) {
-  let finalArray = shuffle(allArrays);
-  console.log(finalArray);
-  let allArraysIndex = 0;
+  let finalDice = shuffle(dice);
+  // console.log(finalArray);
+  let diceIndex = 0;
   for (let a = 0; a < 4; a++) {
     for (let b = 0; b < 4; b++) {
       // let randomDie = allArrays[Math.floor(Math.random() * 16)];
-      let randomChar = finalArray[allArraysIndex].charAt(Math.floor(Math.random() * 6));
+      let randomChar = finalDice[diceIndex].charAt(Math.floor(Math.random() * 6));
       // console.log('Die, char:', randomChar, allArraysIndex);
       board[a][b] = randomChar;
-      allArraysIndex++;
+      diceIndex++;
     }
   }
   return board;
