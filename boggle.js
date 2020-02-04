460fb40b4b3d141fcfea8eee9a4bb0ab0a25b335
Koumpotis Christos <chkoumpotis@davidson.edu>
2020-02-03 19:42:47 -0800

    Gives out a random board with shuffle

--- boggle.js
+++ boggle.js
@@ -1,4 +1,9 @@
-let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+// let alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']; alphabet[Math.floor(Math.random()*26)]
+let allArrays = ['AAEEGN', 'ELRTTY', 'AOOTTW',
+  'ABBJOO', 'EHRTVW', 'CIMOTU', 'DISTTY', 'EIOSST',
+  'DELRVY', 'ACHOPS', 'HIMNQU', 'EEINSU', 'EEGHNW',
+  'AFFKPS', 'HLNNRZ', 'DEILRX'];
+let shuffledArray = [];
 
 function newBoggleBoard() {
   return [
@@ -15,17 +20,56 @@ function printBoggleBoard(boggleBoard) {
   }
 }
 
+// function shuffle(allArrays) {
+//   for (let a = 0; a <= 7; a++) {
+//     let randomDie = Math.floor(Math.random() * 16);
+//     allArrays.push(allArrays[randomDie]);
+//     allArrays.splice(randomDie, 1);
+//   }
+//   console.log(allArrays);
+// }
+
+function randomRange(length) {
+  return Math.floor(Math.random() * length - 1);
+}
+
+function removeElement(allArrays, index) {
+  let item = allArrays.splice(index, 1)[0];
+  return item;
+}
+
+function shuffle(allArrays) {
+  while (allArrays.length > 0) {
+    let randIndex = randomRange(allArrays.length);
+    let removedElem = removeElement(allArrays, randIndex);
+    shuffledArray.push(removedElem);
+   // return shuffledArray;
+    // console.log(removedElem);
+  }
+  return shuffledArray;
+}
 
 function shake(board) {
- for (a=0; a<=15; a++){
-   board[a] = alphabet[Math.floor(Math.random()*26)];
- }
+  let finalArray = shuffle(allArrays);
+  console.log(finalArray);
+  let allArraysIndex = 0;
+  for (let a = 0; a < 4; a++) {
+    for (let b = 0; b < 4; b++) {
+      // let randomDie = allArrays[Math.floor(Math.random() * 16)];
+      let randomChar = finalArray[allArraysIndex].charAt(Math.floor(Math.random() * 6));
+      // console.log('Die, char:', randomChar, allArraysIndex);
+      board[a][b] = randomChar;
+      allArraysIndex++;
+    }
+  }
   return board;
 }
-//console.log(newBoggleBoard);
+
+// console.log(newBoggleBoard);
+
 let board = newBoggleBoard();
 
 shake(board);
-console.log( board);
+console.log(board);
 
-//printBoggleBoard(newBoggleBoard);
+// printBoggleBoard(newBoggleBoard);
